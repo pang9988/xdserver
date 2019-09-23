5bd0a3c02c6f152259f16bc7789d0e48a921ae9f
pang9988 <pzy5021@163.com>
2019-09-23 22:16:06 +0800

    55

---
+++ xdserver/pool/pool.js
@@ -0,0 +1,14 @@
+const mysql=require('mysql');
+//创建数据库连接池
+var pool=mysql.createPool({
+    host:"127.0.0.1",
+    user:"root",
+    password:"",
+    port:3306,
+    connectionLimit:20,
+    database:"xindan"
+})
+ //冻结连接池 不能修改
+ Object.freeze(pool);
+ //导出连接池对象
+   module.exports=pool;
